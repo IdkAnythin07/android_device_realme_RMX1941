a82a011601da2edbb5b2a3f78e2c4e52a3bd068a
IdkAnythin07 <avranil2007rt@gmail.com>
2023-04-05 17:22:19 +0530

    initial octavi bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-	$(LOCAL_DIR)/lineage_RMX1941.mk
+	$(LOCAL_DIR)/octavi_RMX1941.mk
 
 COMMON_LUNCH_CHOICES := \
-        lineage_RMX1941-eng \
-        lineage_RMX1941-user \
-        lineage_RMX1941-userdebug
+        octavi_RMX1941-eng \
+        octavi_RMX1941-user \
+        octavi_RMX1941-userdebug
