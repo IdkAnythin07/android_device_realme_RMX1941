95a632ecdfe20c1a525a0afcb8d300b82d3b32da
IdkAnythin07 <94067465+IdkAnythin07@users.noreply.github.com>
2023-04-10 19:20:30 +0530

    Adapt for evox

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-	$(LOCAL_DIR)/lineage_RMX1941.mk
+	$(LOCAL_DIR)/evolution_RMX1941.mk
 
 COMMON_LUNCH_CHOICES := \
-        lineage_RMX1941-eng \
-        lineage_RMX1941-user \
-        lineage_RMX1941-userdebug
+        evolution_RMX1941-eng \
+        evolution_RMX1941-user \
+        evolution_RMX1941-userdebug
