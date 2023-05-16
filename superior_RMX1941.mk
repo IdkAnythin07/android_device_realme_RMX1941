196350e7e4d91d87f50ecbce1e1f8f8280465b4f
IdkAnythin07 <94067465+IdkAnythin07@users.noreply.github.com>
2023-05-16 21:47:06 +0530

    Change common full phone to common

--- superior_RMX1941.mk
+++ superior_RMX1941.mk
@@ -25,7 +25,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/handheld_system_ext.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/telephony_system_ext.mk)
 
 # Inherit some common Superior OS stuff
-$(call inherit-product, vendor/superior/config/common_full_phone.mk)
+$(call inherit-product, vendor/superior/config/common.mk)
 
 # Inherit from RMX1941 device makefile
 $(call inherit-product, device/realme/RMX1941/device.mk)
